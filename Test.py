826ac22899d181ef258dee4c64a73bb50224de78
EddieWang <eddie1998221@gmail.com>
2019-04-26 20:28:40 +0800

    update Test file

--- Test.py
+++ Test.py
@@ -1,42 +1,43 @@
 #import python的unittest
 import unittest
-import Calculate as calculator
-class TestCalculator(unittest.TestCase):
-    def test_int_add(self):
-        self.assertEqual(calculator.add(9, 3), 12)
+import Calculate as calculate
 
-    def test_int2_add(self):
-        self.assertEqual(calculator.add(9, 4), 13)
+class Test(unittest.TestCase):
+    def test_add1(self):
+        self.assertEqual(calculate.add(10, 3), 13)
 
-    def test_float_add(self):
-        self.assertEqual(calculator.add(4.2, 3), 7.2)
+    def test_add2(self):
+        self.assertEqual(calculate.add(1, 4), 5)
 
-    def test_int_minus(self):
-        self.assertEqual(calculator.minus(9, 3), 6)
+    def test_add3(self):
+        self.assertEqual(calculate.add(4, 4.7), 8.7)
 
-    def test_int2_minus(self):
-        self.assertEqual(calculator.minus(9, 4), 5)
+    def test_minus1(self):
+        self.assertEqual(calculate.minus(4, 1), 3)
 
-    def test_float_minus(self):
-        self.assertEqual(calculator.minus(4.2, 3.2), 1.0)
+    def test_minus2(self):
+        self.assertEqual(calculate.minus(8, 4), 4)
 
-    def test_int_times(self):
-        self.assertEqual(calculator.times(9, 3), 27)
+    def test_minus3(self):
+        self.assertEqual(calculate.minus(8.7, 0.9), 7.8)
 
-    def test_int2_times(self):
-        self.assertEqual(calculator.times(9, 4), 36)
+    def test_times1(self):
+        self.assertEqual(calculate.times(2, 3), 6)
 
-    def test_float_times(self):
-        self.assertEqual(calculator.times(4.2, 3.3), 13.86)
+    def test_times2(self):
+        self.assertEqual(calculate.times(2, 39), 78)
 
-    def test_int_divided(self):
-        self.assertEqual(calculator.divided(9, 3), 3)
+    def test_times3(self):
+        self.assertEqual(calculate.times(3.5, 2.1), 7.35)
 
-    def test_int2_divided(self):
-        self.assertEqual(calculator.divided(9, 0), "無限")
+    def test_divided1(self):
+        self.assertEqual(calculate.divided(9, 3), 3)
 
-    def test_float_divided(self):
-        self.assertEqual(calculator.divided(5.4, 3), 1.8)
+    def test_divided2(self):
+        self.assertEqual(calculate.divided(9, 0), "nan")
+
+    def test_divided3(self):
+        self.assertEqual(calculate.divided(5.4, 3), 1.8)
 
 if __name__ == '__main__':
     unittest.main()
