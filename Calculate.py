d91d8c8bb944fc1dcc8b9c0b9c7cbfd8403b3212
EddieWang <eddie1998221@gmail.com>
2019-04-26 20:29:16 +0800

    update Calculate.py

--- Calculate.py
+++ Calculate.py
@@ -8,10 +8,10 @@ def add(a, b):
 def minus(a, b):
     return a - b
 
-def times(a, b):
+def mult(a, b):
     return a * b
 
-def divided(a, b):
+def div(a, b):
     if b != 0:
         return a / b
-    return "無限"
+    return "nan"
