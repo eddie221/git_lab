eff57fafa1680613cd663b991eeb644adb2dc049
EddieWang <eddie1998221@gmail.com>
2019-04-29 15:34:18 +0800

    update

--- Helloworld/homeweb/views.py
+++ Helloworld/homeweb/views.py
@@ -6,6 +6,6 @@ from django.http import HttpResponse
 
 def index(request):
 	context = {}
-	context['hello'] = "Hello world!!!!"
+	context['hello'] = "Hello world! It is really hard to use this."
     #return HttpResponse("Hello, world. You're at the homeweb index.")
 	return render(request, 'hello.html', context)
