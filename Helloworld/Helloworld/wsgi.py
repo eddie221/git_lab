5af2185d0769fd6ad9db4de475d3f5c47641fe23
EddieWang <eddie1998221@gmail.com>
2019-04-29 15:45:49 +0800

    update new command

--- Helloworld/Helloworld/wsgi.py
+++ Helloworld/Helloworld/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
-
+'''
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,16 +14,16 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Helloworld.settings')
 
 application = get_wsgi_application()
-
+'''
 #the code below is for deploy on heroku
 # mysite/mysite/wsgi.py
 
-#import os
+import os
 
-#from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
-#from dj_static import Cling
+from dj_static import Cling
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Helloworld.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Helloworld.settings")
 
-#application = Cling(get_wsgi_application())
+application = Cling(get_wsgi_application())
